817155cff524394cb6bb4f818ae191b1591b0b86
bengbengku <anggiatbenget70@gmail.com>
2020-10-25 22:45:11 +0700

    ADD: Setup functions api endpoint menggunakan express

--- functions/index.js
+++ functions/index.js
@@ -3,15 +3,10 @@ const admin = require("firebase-admin");
 
 admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello Worlds!");
-});
+const express = require("express");
+const app = express();
 
-exports.getScreams = functions.https.onRequest((req, res) => {
+app.get("/screams", (req, res) => {
   admin
     .firestore()
     .collection("screams")
@@ -48,3 +43,5 @@ exports.createScream = functions.https.onRequest((req, res) => {
       console.error(err);
     });
 });
+
+exports.api = functions.https.onRequest(app);
