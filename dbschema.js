389fa10c9d3df60f69b20c4386404c225f5d8f37
bengbengku <anggiatbenget70@gmail.com>
2020-10-25 23:18:56 +0700

    ADD: Testing post db

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+  screams: [
+    {
+      userHandle: "user",
+      body: "ini adalah bisi dari body scream",
+      createdAt: "2020-10-25T16:14:29.652Z",
+      likeCount: 5,
+      commentCount: 2,
+    },
+  ],
+};
