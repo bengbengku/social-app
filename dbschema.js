5e982632ece43126a2cb436ad8429f1e62de9f6e
bengbengku <anggiatbenget70@gmail.com>
2020-10-27 06:00:02 +0700

    ADD: data users dummy

--- dbschema.js
+++ dbschema.js
@@ -1,4 +1,16 @@
 let db = {
+  users: [
+    {
+      userId: "ixla3jg3pIeT4B2uthHFZFlVZUI2",
+      email: "user@gmail.com",
+      handle: "user",
+      createdAt: "2020-10-25T17:22:04.404Z",
+      imageUrl: "image/jhsadhshdj/ksjfhjhsa",
+      bio: "Hello, saya anggiat benget selamat datang di bio saya",
+      website: "https://user.com",
+      location: "Indonesia, IDN",
+    },
+  ],
   screams: [
     {
       userHandle: "user",
