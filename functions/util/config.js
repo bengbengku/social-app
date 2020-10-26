68654c45987874dea132f69586ad9cf6fe98302f
bengbengku <anggiatbenget70@gmail.com>
2020-10-27 02:12:32 +0700

    Refactoring project

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyCCH_mtOYboZ-wDSUjXaBHNGvkBG5gRxJ4",
+  authDomain: "sosialapp-e25e4.firebaseapp.com",
+  databaseURL: "https://sosialapp-e25e4.firebaseio.com",
+  projectId: "sosialapp-e25e4",
+  storageBucket: "sosialapp-e25e4.appspot.com",
+  messagingSenderId: "540330350753",
+  appId: "1:540330350753:web:53b29588872afdc32d38e0",
+  measurementId: "G-QQ7HTLRYV3",
+};
