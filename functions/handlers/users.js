ec6281503595b11641f6258290170c599b1bcf65
bengbengku <anggiatbenget70@gmail.com>
2020-10-28 05:37:50 +0700

    ADD: getauthuser likes pada db

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -109,6 +109,33 @@ exports.addUserDetails = (req, res) => {
     });
 };
 
+exports.getAuthenticatedUser = (req, res) => {
+  let userData = {};
+
+  db.doc(`/users/${req.user.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        userData.credentials = doc.data();
+        return db
+          .collection("likes")
+          .where("userHandle", "==", req.user.handle)
+          .get();
+      }
+    })
+    .then((data) => {
+      userData.likes = [];
+      data.forEach((doc) => {
+        userData.likes.push(doc.data());
+      });
+      return res.json(userData);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
+
 exports.uploadImage = (req, res) => {
   const BusBoy = require("busboy");
   const path = require("path");
