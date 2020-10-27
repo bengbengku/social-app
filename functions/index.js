ae6121cdbbd7ee57e9d423ed4975f0bee191af48
bengbengku <anggiatbenget70@gmail.com>
2020-10-28 05:48:40 +0700

    ADD: Route untuk post comment scream berdasarkan screamId

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const app = require("express")();
 const FBAuth = require("./util/fbAuth");
-const { getAllScreams, postOneScream } = require("./handlers/screams");
+const { getAllScreams, postOneScream, getScream } = require("./handlers/screams");
 const {
   signup,
   login,
