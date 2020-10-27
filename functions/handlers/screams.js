45a1feeacf40a9e602feba1544754d41cce09d30
bengbengku <anggiatbenget70@gmail.com>
2020-10-28 06:12:05 +0700

    ADD: Fungsi getScream pada field comments

--- functions/handlers/screams.js
+++ functions/handlers/screams.js
@@ -45,3 +45,31 @@ exports.postOneScream = (req, res) => {
       console.error(err);
     });
 };
+
+exports.getScream = (req, res) => {
+  let screamData = {};
+  db.doc(`/screams/${req.params.screamId}`)
+    .get()
+    .then((doc) => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: "Scream not found" });
+      }
+      screamData = doc.data();
+      screamData.screamId = doc.id;
+      return db
+        .collection("comments")
+        .where("screamId", "==", req.params.screamId)
+        .get();
+    })
+    .then((data) => {
+      screamData.comments = [];
+      data.forEach((doc) => {
+        screamData.push(doc.data);
+      });
+      return res.json(screamData);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
