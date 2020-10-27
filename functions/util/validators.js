8068ae6435a0731ce742239c6ada9f85b59e3d1e
bengbengku <anggiatbenget70@gmail.com>
2020-10-28 04:53:02 +0700

    ADD: Validasi pada userDetails

--- functions/util/validators.js
+++ functions/util/validators.js
@@ -1,3 +1,5 @@
+const { user } = require("firebase-functions/lib/providers/auth");
+
 const isEmail = (email) => {
   const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (email.match(regEx)) return true;
@@ -42,5 +44,17 @@ exports.validateLoginData = (data) => {
 };
 
 exports.reduceUserDetails = (data) => {
-  
-}
+  let userDetails = {};
+
+  if (!isEmpty(data.bio.trim())) userDetails.bio = data.bio;
+
+  if (!isEmpty(data.website.trim())) {
+    if (data.website.trim().substring(0, 4) !== "http") {
+      userDetails.website = `https://${data.website.trim()}`;
+    } else userDetails.website = data.website;
+  }
+
+  if (!isEmpty(data.location.trim())) userDetails.location = data.location;
+
+  return userDetails;
+};
