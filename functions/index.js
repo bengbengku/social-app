142abd623db61b9a12ba06b602754a458832d793
bengbengku <anggiatbenget70@gmail.com>
2020-10-28 05:26:52 +0700

    ADD: Route get user auth

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,20 @@
 const functions = require("firebase-functions");
 const app = require("express")();
-const FBAuth = require('./util/fbAuth');
+const FBAuth = require("./util/fbAuth");
 const { getAllScreams, postOneScream } = require("./handlers/screams");
-const { signup, login, uploadImage, addUserDetails } = require("./handlers/users");
+const {
+  signup,
+  login,
+  uploadImage,
+  addUserDetails,
+  getAuthenticatedUser,
+} = require("./handlers/users");
 
 //Screams route
 app.get("/screams", getAllScreams);
+app.get("/user", FBAuth, getAuthenticatedUser);
 app.post("/scream", FBAuth, postOneScream);
-app.post("/user",  FBAuth, addUserDetails)
+app.post("/user", FBAuth, addUserDetails);
 
 // Signup & Login route
 app.post("/signup", signup);
