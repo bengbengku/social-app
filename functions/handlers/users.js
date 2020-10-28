1535e387027bb69e257ff1bbed5a8b41875ca016
bengbengku <anggiatbenget70@gmail.com>
2020-10-28 19:27:27 +0700

    ADD: getUserDetails dari field screams

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -110,12 +110,41 @@ exports.addUserDetails = (req, res) => {
 };
 
 exports.getUserDetails = (req, res) => {
-
-}
-
-exports.markNotificationsRead = (req, res) => {
-  
-}
+  let userData = {};
+  db.doc(`/users/${req.params.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        userData.user = doc.data();
+        return db
+          .collection("screams")
+          .where("userHandle", "==", req.params.handle)
+          .orderBy("createdAt", "desc")
+          .get();
+      } else {
+        return res.status(404).json({ error: "User not found" });
+      }
+    })
+    .then((data) => {
+      userData.screams = [];
+      data.forEach((doc) => {
+        userData.screams.push({
+          body: doc.data().body,
+          createdAt: doc.data().createdAt,
+          userHandle: doc.data().userHandle,
+          userImage: doc.data().userImage,
+          likeCount: doc.data().likeCount,
+          commentCount: doc.data().commentCount,
+          screamId: doc.id,
+        });
+      });
+      return res.json(userData);
+    })
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).json({ error: err.code });
+    });
+};
 
 exports.getAuthenticatedUser = (req, res) => {
   let userData = {};
