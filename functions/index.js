99008900759621ef768cd5370e703fe3cb21feb3
bengbengku <anggiatbenget70@gmail.com>
2020-10-28 15:24:19 +0700

    ADD: Route untuk delete scream

--- functions/index.js
+++ functions/index.js
@@ -8,6 +8,7 @@ const {
   commentOnScream,
   likeScream,
   unlikeScream,
+  deleteScream,
 } = require("./handlers/screams");
 
 const {
@@ -22,6 +23,7 @@ const {
 app.get("/screams", getAllScreams);
 app.post("/scream", FBAuth, postOneScream);
 app.get("/scream/:screamId", getScream);
+app.delete("/scream/:screamId", FBAuth, deleteScream);
 app.get("/scream/:screamId/like", FBAuth, likeScream);
 app.get("/scream/:screamId/unlike", FBAuth, unlikeScream);
 app.post("/scream/:screamId/comment", FBAuth, commentOnScream);
