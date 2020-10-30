dabf34813e9e19ac09f5a1fb6ab93d41896aa11b
bengbengku <anggiatbenget70@gmail.com>
2020-10-31 04:10:35 +0700

    Fix error

--- dbschema.js
+++ dbschema.js
@@ -14,7 +14,7 @@ let db = {
   screams: [
     {
       userHandle: "user",
-      body: "ini adalah bisi dari body scream",
+      body: "ini adalah isi dari body scream",
       createdAt: "2020-10-25T16:14:29.652Z",
       likeCount: 5,
       commentCount: 2,
--- functions/util/validators.js
+++ functions/util/validators.js
@@ -1,5 +1,3 @@
-const { user } = require("firebase-functions/lib/providers/auth");
-
 const isEmail = (email) => {
   const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   if (email.match(regEx)) return true;
